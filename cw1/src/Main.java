1374e9fdf29537a7d54b04c08ddcc741aef6773c
Artur <artur.kaminskii@o2.pl>
2025-03-06 11:01:03 +0100

    commit3

--- cw1/src/Main.java
+++ cw1/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("eloeleo");
         System.out.println("eloeleoqweqweqwe");
+        System.out.println("eloe");
     }
 }
